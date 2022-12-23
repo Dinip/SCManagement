1335405f5fa06cf5eed1ccc0725ba5441a1e55b0
Amatias13 <80266371+Amatias13@users.noreply.github.com>
2022-12-23 17:06:42 +0000

    Date of birth validater

---
+++ SCManagement/Models/Validations/DateOfBirthAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using SCManagement.Services;
+
+namespace SCManagement.Models.Validations
+{
+    public class DateOfBirthAttribute : ValidationAttribute
+    {
+        public int MinAge { get; set; }
+        public int MaxAge { get; set; }
+
+        private string GetErrorMessage(ValidationContext validationContext)
+        {
+            SharedResourceService errorTranslation = validationContext.GetService(typeof(SharedResourceService)) as SharedResourceService;
+            return string.Format(errorTranslation.Get("Error_DateOfBirth"), MinAge, MaxAge);
+        }
+
+        protected override ValidationResult? IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+                return ValidationResult.Success;
+
+            var val = (DateTime)value;
+
+            if (val.AddYears(MinAge) > DateTime.Now)
+                return new ValidationResult(GetErrorMessage(validationContext));
+
+            if (val.AddYears(MaxAge) < DateTime.Now)
+                return new ValidationResult(GetErrorMessage(validationContext));
+
+            return ValidationResult.Success;
+
+        }
+    }
+}
