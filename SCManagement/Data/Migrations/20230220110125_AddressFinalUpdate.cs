8ce783ac5e75bd54cbc73c38a76198a067cccb7c
DavidBelchior <david10aturma@gmail.com>
2023-02-20 11:07:22 +0000

    Address Fixed

---
+++ SCManagement/Data/Migrations/20230220110125_AddressFinalUpdate.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SCManagement.Data.Migrations
+{
+    public partial class AddressFinalUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "CoordinateY",
+                table: "Address",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "CoordinateX",
+                table: "Address",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "CoordinateY",
+                table: "Address",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "CoordinateX",
+                table: "Address",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
--- SCManagement/Models/Address.cs
+++ SCManagement/Models/Address.cs
@@ -6,8 +6,8 @@ namespace SCManagement.Models
     public class Address
     {
         public int Id { get; set; }
-        public double? CoordinateX { get; set; }
-        public double? CoordinateY { get; set; }
+        public double CoordinateX { get; set; }
+        public double CoordinateY { get; set; }
         public string? ZipCode { get; set; }
         public string Street { get; set; }
         public string City { get; set; }
