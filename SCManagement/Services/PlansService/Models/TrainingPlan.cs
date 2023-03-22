d04dfc9fc9de5676a3ff6311cd67a73aab841011
Amatias13 <80266371+Amatias13@users.noreply.github.com>
2023-03-22 16:13:57 +0000

    fix

--- SCManagement/Services/PlansService/Models/TrainingPlan.cs
+++ SCManagement/Services/PlansService/Models/TrainingPlan.cs
@@ -9,7 +9,6 @@ namespace SCManagement.Services.PlansService.Models
     public class TrainingPlan : Plan
     {
         [Display(Name = "Modality")]
-        [Required(ErrorMessage = "Error_Required")]
         public Modality? Modality { get; set; }
         public int ModalityId { get; set; }
         public ICollection<TrainingPlanSession>? TrainingPlanSessions { get; set; }
